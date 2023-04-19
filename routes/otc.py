23198b3966163189167ff1afa30ed90eccfa84ca
Nahuel Pereira <yo@devpbeat.rocks>
2023-04-19 18:06:17 -0400

    fix

--- routes/otc.py
+++ routes/otc.py
@@ -14,29 +14,31 @@ def index():
 @routingOTC.route('/create', methods=['GET', 'POST'])
 def create():
     form = OTCForm()
-    if form.validate_on_submit():
+    print(f"este es el formulariooooooooooooo {form.errors}")
+    if request.method == 'GET':
+        return render_template('otc/create.html', form=form)
+    elif request.method == 'POST' and form.validate_on_submit():
         with db_session:
             persona = PersonaPrivadaDeLibertad[form.id_persona_privada.data]
-            print({'persona': persona})
             otc = OrganismoTecnicoCriminologico(
                 id_persona_privada=persona.id_persona_privada,
                 fecha_evaluacion=form.fecha_evaluacion.data,
                 riesgo=form.riesgo.data,
                 observaciones=form.observaciones.data
             )
-            flush()
-        return redirect(url_for('otc.index'))
-    return render_template('otc/create.html', form=form)
-
+            db.commit()
+            return redirect(url_for('otc.index'))
+    elif request.method == 'POST' and not form.validate_on_submit():
+        return f'formulario no valido {form.errors}'
 @routingOTC.route('/update/<id>', methods=['GET', 'POST'])
 def update(id):
     # obtener el objeto a actualizar y pasarle los datos al formulario
-    otc = OrganismoTecnicoCriminologico[id]
+    otc = OrganismoTecnicoCriminologico[int(id)]
     otc.id_persona_privada = otc.id_persona_privada.id_persona_privada
     form = OTCForm(obj=otc)
 
     if request.method == 'GET':
-        return render_template('otc/update.html', form=form, id=id)
+        return render_template('otc/update.html', form=form)
     elif request.method == 'POST'and form.validate_on_submit():
             with db_session:
                 persona = PersonaPrivadaDeLibertad[form.id_persona_privada.data]
@@ -46,6 +48,8 @@ def update(id):
                 otc.observaciones = form.observaciones.data
                 db.commit()
                 return redirect(url_for('otc.index'))
+    elif request.method == 'POST' and not form.validate_on_submit():
+        return f'formulario no valido {form.errors}'
 
 @routingOTC.route('/delete/<id>', methods=['GET', 'POST'])
 def delete(id):
